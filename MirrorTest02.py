4a523f85c56efad4809bce8516e53c31140e8cdf
John <overwhelm07@gmail.com>
2015-11-29 17:07:39 +0900

    TED HTML소스 뷰티플소프를 이용해서 파싱하기 성공!

--- MirrorTest02.py
+++ MirrorTest02.py
@@ -10,11 +10,12 @@ import urllib
 from bs4 import BeautifulSoup
 
 
-html = urllib.urlopen('http://comic.naver.com/webtoon/weekday.nhn')
+html = urllib.urlopen('http://tedxtalks.ted.com/')
 soup = BeautifulSoup(html, "lxml")
-titles = soup.find_all("a", "title")
+titles = soup.find_all("div", "mvp_block")
 
 print titles
+#print(soup.prettify())
 '''  
 for title in titles:
     print 'title:{0:10s} link:{1:20s}\n'.format(title['title'].encode('utf-8'), title['href'].encode('utf-8'))
